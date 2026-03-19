086ed2a5f84928bf2c018750a12cabc9e3af0118
Shajedul Kabir Rafi <shajidulkabir12345@gmail.com>
2026-03-20 03:25:58 +0600

    chore: update app/Models/JobApplication.php

--- app/Models/JobApplication.php
+++ app/Models/JobApplication.php
@@ -14,10 +14,31 @@ class JobApplication extends Model
         'job_id',
         'status',
         'applied_at',
+        'personal_info',
+        'resume_path',
+        'work_experience',
+        'education_info',
+        'skills',
+        'cover_letter',
+        'additional_documents',
+        'work_eligibility',
+        'references',
+        'online_profiles',
+        'application_notes',
+        'submitted_at',
     ];
 
     protected $casts = [
         'applied_at' => 'datetime',
+        'submitted_at' => 'datetime',
+        'personal_info' => 'array',
+        'work_experience' => 'array',
+        'education_info' => 'array',
+        'skills' => 'array',
+        'additional_documents' => 'array',
+        'work_eligibility' => 'array',
+        'references' => 'array',
+        'online_profiles' => 'array',
     ];
 
     public function user()
@@ -28,5 +49,10 @@ class JobApplication extends Model
     public function job()
     {
         return $this->belongsTo(Job::class);
+    }
+
+    public function screeningResponses()
+    {
+        return $this->hasMany(ScreeningResponse::class);
     }
 }
