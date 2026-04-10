9d0af6e57ffe37740fcff5bb4978814be76d8c01
Rafi12234 <shajidulkabir12345@gmail.com>
2026-04-10 06:19:46 +0600

    Update app/Models/UserSession.php

---
+++ app/Models/UserSession.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserSession extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'jti',
+        'device_name',
+        'issued_at',
+        'expires_at',
+        'revoked_at',
+    ];
+
+    protected $casts = [
+        'issued_at' => 'datetime',
+        'expires_at' => 'datetime',
+        'revoked_at' => 'datetime',
+    ];
+}
